b67361bde075236d925a4627cd0a9fb76b4b4b7a
comstock <comstock@users.noreply.github.com>
2016-03-02 15:12:36 -0500

    Update shell_script_to_record_fileSizes.sh

--- shell_script_to_record_fileSizes.sh
+++ shell_script_to_record_fileSizes.sh
@@ -1,3 +1,16 @@
-ls -l -h -S -R -G * | awk '{if ($3 == "comstock")
+#!/bin/bash
+# Generate a listing of files and directories
+# Send the listing directly to AWK
+# Generate a file that includes a listing
+# for each directory, and the directory's size
+# and a listing of files within the directory,
+# sorted largest to smallest.
+#
+# BUG : When a name includes a blank space it will think the name spans multiple columns
+#
+ls -l --human-readable -S --quote-name --recursive --no-group * | awk '{
+if($3 == "comstock")
 print $4 "\t" $8;
-else if(print $0):}'
+else
+print $0;}'
+# > directory_listing.txt
