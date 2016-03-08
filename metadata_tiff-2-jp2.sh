151d982bc5e21d439c961589534990d6a3ca5ccb
comstock <comstock@users.noreply.github.com>
2016-03-08 09:22:58 -0500

    Delete metadata_tiff-2-jp2.sh

--- metadata_tiff-2-jp2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# copy the image technical metadata from source
-# TIFF file to a derivative JP2
-#
-while read line
-do exiftool -@ ImageHistory.txt -TagsFromFile $line".tif" $line".jp2"
-echo $line
-done < singleColumnList_TIFFs.txt
