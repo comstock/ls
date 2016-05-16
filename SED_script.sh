814c1f6716d71587f6f3777ae1a500ede62b4abb
comstock <comstock@users.noreply.github.com>
2016-05-16 15:39:21 -0400

    Update SED_script.sh

--- SED_script.sh
+++ SED_script.sh
@@ -7,7 +7,7 @@ ls -1 *.xml | sed -r 's/\.xml$//g' | sort -n > singleColumnList_XMLs.txt
 while read line
 do
 mv $line.xml $line.input
-sed -e 's/\/run\/user\/1000\/gvfs\/smb-share:server=pentos-smb.ad.hcl.harvard.edu,share=digilab/\/GENERIC_SAN_VOL_NAME/g' $line.input > $line.xml
+sed -e 's/\/run\/user\/1000\/gvfs\/smb-share:server=jellybean.harvard.edu,share=digilab/\/GENERIC_SAN_VOL_NAME/g' $line.input > $line.xml
 echo ""
 echo $line".xml"
 done < singleColumnList_XMLs.txt
