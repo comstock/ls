167a4fdf65a365c9d9c8929b8f6bcb2fe1c393b1
comstock <comstock@users.noreply.github.com>
2018-02-08 07:55:33 -0500

    Update and rename openKPEG_convert_dir_JP2.sh to openjpeg_convert_dir_JP2.sh

---
+++ openjpeg_convert_dir_JP2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# 1. generate a list of image files in dir
+# 2. make a file extension variable
+# 3. make a file prefix variable
+# 4. generate a jp2 file from each image file
+#    keeping the same file prefix
+# 5. Output indicators of progress in std out
+# 6. Delete list of files
+#
+inFILE="fileList.txt"
+ls -1 | grep --ignore-case --extended-regexp '(tif$|tiff$|jpg$|jpeg$|png$)' > $inFILE
+#
+START=`date +%s`
+while read line
+do
+    EXT=$(echo $line |  grep -o --ignore-case --extended-regexp '[a-z]{3,4}$')
+    PRE=$(echo $line | sed --regexp-extended 's/\.'$EXT'//g')
+    # do opj_compress -i $line -o $PRE".jp2" -p RLCP -t 1024,1024 -EPH -SOP -OutFor jp2
+    opj_compress -i $line -o $PRE".jp2" -p RLCP -t 1024,1024 -EPH -SOP -OutFor jp2 -I -q 30
+    echo "Extension = "$EXT
+    echo "Prefix = "$PRE
+    echo "Line = "$line
+done < $inFILE
+END=`date +%s`
+RUNTIME=$((END - START))
+echo "Seconds ="$RUNTIME
+rm -f $inFILE
