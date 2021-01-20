5c33cf6622bb3359b7d45ccda3aa06dca080a012
comstock <comstock@users.noreply.github.com>
2021-01-20 09:57:07 -0500

    Create grk_compress_TIFF_to_JP2_lossy.sh

---
+++ grk_compress_TIFF_to_JP2_lossy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Grok Image Compression JP2 JPEG2000 codec encoder lossy compression
+# https://github.com/GrokImageCompression/grok
+find . -type f | grep --extended-regexp ".*\.tif$" | sed -E "s/.tif//g" > tiffNameStem.txt
+ls -1 *.tif | wc -l | xargs echo "TIFF count: "
+while read line
+do
+#    grk_compress -i $line".tif" -o $line".jp2" -p RLCP -t 1024,1024 -EPH -SOP -OutFor jp2 -I -q 42                   
+    grk_compress -i $line".tif" -o $line"_lossy.jp2" -p RLCP -t 1024,1024 -EPH -SOP -OutFor jp2 -I -q 42
+    echo "converting "$line".tif"
+done < tiffNameStem.txt
+echo "                                                                                                                
+     ~~~ FIN ~~~                                                                                                      
+"
+ls -1 *.jp2 | wc -l | xargs echo "JP2 count: "
