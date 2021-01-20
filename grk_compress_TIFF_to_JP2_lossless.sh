910ec570b5a28020036e51de207fb0afc4e6cd40
comstock <comstock@users.noreply.github.com>
2021-01-20 08:59:16 -0500

    Create grk_compress_TIFF_to_JP2_lossless.sh

---
+++ grk_compress_TIFF_to_JP2_lossless.sh
@@ -0,0 +1,13 @@
+#!/bin/bash                                                                                                           
+find . -type f | grep --extended-regexp ".*\.tif$" | sed -E "s/.tif//g" > tiffNameStem.txt
+ls -1 *.tif | wc -l | xargs echo "TIFF count: "
+while read line
+do
+#    grk_compress -i $line".tif" -o $line".jp2" -p RLCP -t 1024,1024 -EPH -SOP -OutFor jp2                            
+    grk_compress -i $line".tif" -o $line"_lossless.jp2" -p RLCP -t 1024,1024 -EPH -SOP -OutFor jp2
+    echo "converting "$line".tif"
+done < tiffNameStem.txt
+echo "                                                                                                                
+     ~~~ FIN ~~~                                                                                                      
+"
+ls -1 *.jp2 | wc -l | xargs echo "JP2 count: "
